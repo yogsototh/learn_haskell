a67ba9543b3fbae2e55dbd4b6a63610824283cb7
Yann Esposito <yann.esposito@gmail.com>
2012-10-04 12:06:01 +0200

    added all part for russian translation

--- create_ymd.sh
+++ create_ymd.sh
@@ -26,8 +26,8 @@ en: %tldr A very short and dense tutorial for learning Haskell.
 
 fr: %tlal Un tutoriel très court mais très dense pour apprendre Haskell.
 
-en: [A Russian version](http://habrahabr.ru/post/152889/) ; thanks to [Oleg Taykalo](https://plus.google.com/u/0/113751420744109290534)!
-fr: [Une version Russe](http://habrahabr.ru/post/152889/) ; merci à [Oleg Taykalo](https://plus.google.com/u/0/113751420744109290534)!
+en: Thanks to [Oleg Taykalo](https://plus.google.com/u/0/113751420744109290534) you can find a Russian translation here: [Part 1](http://habrahabr.ru/post/152889/) _&_ [Part 2](http://habrahabr.ru/post/153383/) ; 
+en: Merci à [Oleg Taykalo](https://plus.google.com/u/0/113751420744109290534) vous pouvez trouver une traduction Russe ici: [Partie 1](http://habrahabr.ru/post/152889/) _&_ [Partie 2](http://habrahabr.ru/post/153383/) ; 
 
 > <center><hr style="width:30%;float:left;border-color:#CCCCD0;margin-top:1em"/><span class="sc"><b>Table of Content</b></span><hr style="width:30%;float:right;border-color:#CCCCD0;margin-top:1em"/></center>
 >
